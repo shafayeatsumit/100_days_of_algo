02f2ce5ab728629198f6af905c2a2c86070b28a9
shafayeatsumit <shafayeatkabir@gmail.com>
2019-09-11 14:53:56 +0600

    meanhep implemented

--- heap/mean_heap.js
+++ heap/mean_heap.js
@@ -17,7 +17,38 @@ var MedianFinder = function () {
   this.minHeap = new Heap([], null, (a,b) => (b-a)) // second half of the numbers (small numbers)
 };
 
-MedianFinder.prototype.addNum = function (num) {
 
+MedianFinder.prototype.addNum = function (num) {
+  if(this.maxHeap.length === 0 || this.maxHeap.peek()>num){
+    this.maxHeap.push(num)
+  }else{
+    this.minHeap.push(num)
+  }
+  if(this.maxHeap.length - this.minHeap.length > 1){
+    this.minHeap.push(this.maxHeap.pop())
+  }else if(this.maxHeap.length - this.minHeap.length < 0){
+    this.maxHeap.push(this.minHeap.pop())
+  }
+}
+
+
+MedianFinder.prototype.findMedian = function () {
+  if(this.maxHeap.length > this.minHeap){
+    return this.maxHeap.content.shift()
+  }else{
+    let mean = (this.maxHeap.content.shift() + this.minHeap.content.shift())/2
+    return mean
+  }
 };
+
+
+var obj = new MedianFinder()
+obj.addNum(3)
+obj.addNum(1)
+obj.addNum(8)
+obj.addNum(5)
+obj.addNum(11)
+obj.addNum(2)
+obj.addNum(7)
+obj.findMedian()
 
