874a7461f902fe250b8de84b13e4fdb1dd9e4649
shafayeatsumit <shafayeatkabir@gmail.com>
2019-09-17 12:46:19 +0600

    linked list cycle detection

---
+++ linked_list/cycle.js
@@ -0,0 +1,36 @@
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+const has_cycle = function (head) {
+  let fast = head,
+  slow = head;
+  // fast != null covers empty linked list condition
+  // if fast.next is not null then we don't get error in fast.next.next
+  while (fast !== null && fast.next !== null){    
+    slow = slow.next
+    fast = fast.next.next
+    if(fast === slow){      
+      return true
+    }
+  }  
+  return false
+}
+
+
+head = new Node(1)
+head.next = new Node(2)
+head.next.next = new Node(3)
+head.next.next.next = new Node(4)
+head.next.next.next.next = new Node(5)
+head.next.next.next.next.next = new Node(6)
+console.log(`LinkedList has cycle: ${has_cycle(head)}`)
+
+head.next.next.next.next.next.next = head.next.next
+console.log(`LinkedList has cycle: ${has_cycle(head)}`)
+
+head.next.next.next.next.next.next = head.next.next.next
+console.log(`LinkedList has cycle: ${has_cycle(head)}`)
