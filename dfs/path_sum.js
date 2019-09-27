451eac2be7cbb785c29724d75fa1b5c0365ca58a
shafayeatsumit <shafayeatkabir@gmail.com>
2019-09-27 22:39:32 +0600

    dfs path sum

--- dfs/path_sum.js
+++ dfs/path_sum.js
@@ -1,4 +1,7 @@
 /**
+ * Given a binary tree and a sum, determine if the tree has 
+ * a root-to-leaf path such that adding up all 
+ * the values along the path equals the given sum.
  * path sum leet code:112
  */
 
@@ -10,7 +13,21 @@ function TreeNode(val) {
 }
 
 var hasPathSum = function (root, sum) {
-    if(!root) return false
-    if(!root.left && !root.right && root.val === sum) return true
-    return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val)
+  if(!root) return false 
+    
+   if(!root.left && !root.right && sum===root.val){     
+      return true
+   }
+   return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val)    
 };
+
+var root = new TreeNode(1)
+root.left = new TreeNode(2)
+root.right = new TreeNode(3)
+root.left.left = new TreeNode(4)
+root.left.right = new TreeNode(5)
+root.right.left = new TreeNode(6)
+root.right.right = new TreeNode(7)
+
+const result = hasPathSum(root,10)
+console.log(result)
