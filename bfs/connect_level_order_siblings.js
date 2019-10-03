ce1c94809fbc1c1a2165c5d5257c7b253135d573
shafayeatsumit <shafayeatkabir@gmail.com>
2019-10-03 12:33:41 +0600

    bfs connect sigling

---
+++ bfs/connect_level_order_siblings.js
@@ -0,0 +1,55 @@
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+    this.next = null;
+  }
+
+  // level order traversal using 'next' pointer
+  print_level_order() {
+    console.log("Level order traversal using 'next' pointer: ");
+    let nextLevelRoot = this;
+    while (nextLevelRoot !== null) {
+      let current = nextLevelRoot;
+      nextLevelRoot = null;
+      while (current != null) {
+        process.stdout.write(`${current.val} `);
+        if (nextLevelRoot === null) {
+          if (current.left !== null) {
+            nextLevelRoot = current.left;
+          } else if (current.right !== null) {
+            nextLevelRoot = current.right;
+          }
+        }
+        current = current.next;
+      }
+      console.log();
+    }
+  }
+};
+
+const connect_level_order_siblings = function (root) {
+  let Q = [root]
+  while(Q.length>0){
+    let levelLength = Q.length;    
+    for(let i =0; i<levelLength; i++){
+      let node = Q.shift()
+      let nextSibling = Q[0]      
+      if(nextSibling && i !== levelLength-1) node.next = nextSibling
+      if(node.left) Q.push(node.left)
+      if(node.right) Q.push(node.right)
+    }
+  }
+};
+
+
+var root = new TreeNode(12);
+root.left = new TreeNode(7);
+root.right = new TreeNode(1);
+root.left.left = new TreeNode(9);
+root.right.left = new TreeNode(10);
+root.right.right = new TreeNode(5);
+connect_level_order_siblings(root);
+
+root.print_level_order()
