bc199168ff8011461ba4a7f44bd0880f6be6bd0c
shafayeatsumit <shafayeatkabir@gmail.com>
2019-10-09 14:42:53 +0600

    reverse sub list solved

--- reverse_linked_list/reverse_sub_list.js
+++ reverse_linked_list/reverse_sub_list.js
@@ -17,48 +17,49 @@ class Node {
 };
 
 const reverse_sub_list = function (head, m, n) {
-  if(!head || !head.next) return head
-  if(m===n) return head
+  if(!head || !head.next || m===n) return head
   let count = 1,
   left = null,
   right = null;
   let node = head
+
   while(node){
-    if(count === m-1){
+    if(count === m-1 ){
       left = node
+      
     }
     if(count === n){
       right = node.next
-      node.next = null
-      break
+      node.next = null      
     }
     node = node.next
-    count += 1;
-  }  
-  let prev = right,
-  curr = left  ? left.next : null
-  
-  while(curr){
-    let nextNode = curr.next
-    curr.next = prev
-    prev = curr 
-    curr = nextNode
+    count = count +1
   }
-  if (left){
-    left.next = null
+  
+  let current = left? left.next : head;  
+  let prev = right;
+  while(current){
+    let next = current.next;
+    current.next = prev
+    prev = current
+    current = next
+  }
+  
+  if(left){
     left.next = prev
-  } 
+    prev = head
+  }
   
   
-  return left || head;
+  return  prev;
 };
 
 
 head = new Node(1)
 head.next = new Node(2)
-// head.next.next = new Node(3)
-// head.next.next.next = new Node(4)
-// head.next.next.next.next = new Node(5)
-
-console.log(`Nodes of original LinkedList are: ${head.get_list()}`)
-console.log(`Nodes of reversed LinkedList are: ${reverse_sub_list(head, 1, 2).get_list()}`)
+head.next.next = new Node(3)
+head.next.next.next = new Node(4)
+head.next.next.next.next = new Node(5)
+reverse_sub_list(head, 3, 4)
+// console.log(`Nodes of original LinkedList are: ${head.get_list()}`)
+// console.log(`Nodes of reversed LinkedList are: ${reverse_sub_list(head, 1, 2).get_list()}`)
