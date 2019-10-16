e9d43a679a6d2ba2eee6640cc84dd4e064833ba6
shafayeatsumit <shafayeatkabir@gmail.com>
2019-10-16 11:54:45 +0600

    total_sum_of_trees dfs

--- dfs/total_sum_of_tree.js
+++ dfs/total_sum_of_tree.js
@@ -1,5 +1,10 @@
-class TreeNode {
+/**
+ * Given a binary tree where each node can only have a digit (0-9) value, 
+ * each root-to-leaf path will represent a number. 
+ * Find the total sum of all the numbers represented by all paths.
+ */
 
+class TreeNode {
   constructor(value) {
     this.val = value;
     this.left = null;
@@ -8,18 +13,16 @@ class TreeNode {
 };
 
 
-const find_sum_of_path_numbers = function (root) {
-  let sum = 0,
-  num = 0;
-  function findTotalSumRecursive(node,num){
-    if(!node) return 
+const find_sum_of_path_numbers = function (root) {  
+  let num = 0,
+  sum=0;
+  function findTotalSumRecursive(node,num, sum){
+    if(!node) return 0
     num = (num*10) + node.val;    
-    if(!node.left && !node.right) sum += num
-    return findTotalSumRecursive(node.left,num) + findTotalSumRecursive(node.right,num)
+    if(!node.left && !node.right) return sum +=num       
+    return findTotalSumRecursive(node.left,num, sum) + findTotalSumRecursive(node.right,num, sum)
   }
-  findTotalSumRecursive(root,num,sum)
-  
-  return sum
+  return findTotalSumRecursive(root, num, sum)         
 };
 
 
@@ -30,4 +33,5 @@ root.right = new TreeNode(1)
 root.left.left = new TreeNode(1)
 root.right.left = new TreeNode(6)
 root.right.right = new TreeNode(5)
-find_sum_of_path_numbers(root)
+let result = find_sum_of_path_numbers(root)
+console.log(result)
