8a5c93e722997a9802d28ed6d276b1538cbbfa1d
shafayeatsumit <shafayeatkabir@gmail.com>
2019-10-26 15:38:43 +0600

    comment added in insertion sort

--- sort/inseriton_sort.js
+++ sort/inseriton_sort.js
@@ -1,9 +1,12 @@
+// insertion sort: do the right shift if the current value in sorted array
+// is greater than Current Taken NUMBER in the main array.
+// or simply create hole and right shift the bigger numbers.
 function insertionSort(arr){
   for(let i=0; i<arr.length; i++){
     let number = arr[i];
-    let hole = i
+    let hole = i;
     while(hole>0 && arr[hole-1]> number){
-      arr[hole] = arr[hole-1]
+      arr[hole] = arr[hole-1] // arr[hole-1] if it is greater than current number , then right shift.
       hole -=1;
     }
     arr[hole] = number;
