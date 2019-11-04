36744fcb2f2959ba09f917ce448059289d55d639
shafayeatsumit <shafayeatkabir@gmail.com>
2019-11-04 15:37:39 +0600

    modified insertion sort

--- sort/inseriton_sort.js
+++ sort/inseriton_sort.js
@@ -5,7 +5,7 @@ function insertionSort(arr){
   for(let i=0; i<arr.length; i++){
     let number = arr[i];
     let hole = i;
-    while(hole>0 && arr[hole-1]> number){
+    while(hole>0 && arr[hole-1]> number){ // why do we need hole > 0, cause at one point hole would be -1 and arr[-1] => undefined 
       arr[hole] = arr[hole-1] // arr[hole-1] if it is greater than current number , then right shift.
       hole -=1;
     }
@@ -14,5 +14,5 @@ function insertionSort(arr){
   return arr;
 }
 
-let sample = [7,2,4,1,5,3];
+let sample = [7];
 insertionSort(sample)
