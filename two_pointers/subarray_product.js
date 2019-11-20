22681ee1b32bfcac24ff611d8e091961ae5f2968
shafayeatsumit <shafayeatkabir@gmail.com>
2019-11-20 15:02:09 +0600

    two pointers subarray with Product Less than a Target

---
+++ two_pointers/subarray_product.js
@@ -0,0 +1,34 @@
+/**
+ * Given an array with positive numbers and a target number, 
+ * find all of its contiguous subarrays whose product is less than the target number.
+ * Input: [2, 5, 3, 10], target=30
+ * Output: [2], [5], [2, 5], [3], [5, 3], [10]
+ * Input: [8, 2, 6, 5], target=50
+ * Output: [8], [2], [8, 2], [6], [2, 6], [5], [6, 5]
+ */
+
+const find_subarrays = function (arr, target) {
+  let result = [];
+  let windowStart = 0;
+  let product = 1;
+  for(let windowEnd=0; windowEnd<arr.length; windowEnd++){
+    product = product * arr[windowEnd]    
+    while(product>=target){
+      product = product/arr[windowStart];
+      windowStart++;      
+    }        
+    const tempList = [];
+    // since the product of all numbers from left to right is less than the target therefore,
+    // all subarrays from lef to right will have a product less than the target too; to avoid
+    // duplicates, we will start with a subarray containing only arr[right] and then extend it
+    for (let i = windowEnd; i > windowStart - 1; i--) {      
+      tempList.unshift(arr[i]);
+      result.push(tempList.slice());
+    }        
+  }  
+  return result;
+};
+
+let array = [2, 5, 3, 10]
+let result = find_subarrays(array, 30)
+console.log(result)
