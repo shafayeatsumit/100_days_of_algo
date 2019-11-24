b494fc006e667f282dc56ab05441f22b839d6c98
shafayeatsumit <shafayeatkabir@gmail.com>
2019-11-24 14:37:15 +0600

    tree boundry dfs&bfs combination

--- bfs/tree_boundry.js
+++ bfs/tree_boundry.js
@@ -3,45 +3,47 @@
  */
 
 class TreeNode {
-  constructor(value) {
-    this.value = value;
+  constructor(val) {
+    this.val = val;
     this.left = null;
     this.right = null;
   }
 };
 
 const find_tree_boundary = function (root) {
-  var result = [],
-  Q = [root];
-  let level = 0;
+  if (!root) return [];  
+  let Q = [root];
+  let leftView = [];
+  let rightView = [];  
+  let leafs = [];
   while(Q.length){
-    var levelSize = Q.length;    
-    
-    for(let i=0, n =level ; i < levelSize; i++){
+    let levelSize = Q.length;       
+    for(let i=0; i<levelSize; i++){
       let node = Q.shift();
-      debugger
-      if (!node.left && !node.right){
-        result.splice(n,0, node.value)
-        n++
-        //result.push(node.value)
-        continue
-      }
-      if(i===0){
-        result.splice(n, 0, node.value)
-        n++
-
-      }else if(i+1 === levelSize){
-        result.splice(n, 0, node.value)
-        n++
-      }        
+      if(!node.left && !node.right) continue
+      if(i==0){
+        leftView.push(node.val)
+      }else if(i===levelSize-1){
+        rightView.unshift(node.val)
+      }      
       if(node.left) Q.push(node.left)
       if(node.right) Q.push(node.right)
     }    
-    level += 1;    
   }    
-  return result;
+  let leafNodes = findLeafs(root)
+  return [...leftView , ...leafNodes, ...rightView];
 };
 
+function findLeafs(root){
+  let leafs = [];
+  function helper(node) {
+    if(!node.left && !node.right) leafs.push(node.val)
+    if(node.left) helper(node.left)
+    if(node.right) helper(node.right)
+  }
+  helper(root)
+  return leafs;
+}
 
 
 const root = new TreeNode(12);
@@ -54,7 +56,6 @@ root.left.right.left = new TreeNode(15);
 root.right.left = new TreeNode(10);
 root.right.right = new TreeNode(5);
 root.right.right.left = new TreeNode(6);
-result = find_tree_boundary(root);
 
 let r = find_tree_boundary(root)
 console.log(r)
